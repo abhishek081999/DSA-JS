00d48d3dae59bb2b2c651346c8becb63d15b88e8
Abhishek Kumar <100945559+abhishek081999@users.noreply.github.com>
2024-10-29 13:56:05 +0530

    js added

---
+++ js-basics/datatypersdemo.js
@@ -0,0 +1,22 @@
+let productPrice = 150000;
+
+let productDiscount = 15; 
+
+let productName = "Apple Iphone 11 Pro max (512 GB, Blue)";
+
+let isAvailable = false;
+
+let minusZero = -0;
+
+console.log(minusZero);
+
+let x;
+
+let y = null;
+
+console.log(x);
+
+
+let veryBigNumber = 57n;
+
+console.log(veryBigNumber);
---
+++ js-basics/loops/breakdemo.js
@@ -0,0 +1,17 @@
+let i = 1;
+
+// while(j < 100) {
+//     if(j > 90) break;
+    
+    while(i <= 10) {
+
+        if(i % 7 == 0) // if i is divisible by 7
+        {
+            break;
+        }
+        console.log(i);
+        i += 1;
+    }
+// }
+
+console.log("end");
